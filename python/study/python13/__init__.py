4f9e4a49b75fcf95fac2e107365023c8387fe5e3
zhangfengwe <2372590227@qq.com>
2019-08-11 11:23:00 +0800

    python13-Oracle数据库操作优化

--- python/study/python13/__init__.py
+++ python/study/python13/__init__.py
@@ -4,7 +4,10 @@
 import cx_Oracle
 import os
 import sqlite3
+import traceback
 '''
     导入模块
 '''
-__all__ = ['cx_Oracle', 'os', 'sqlite3']
+
+# traceback 异常信息打印模块
+__all__ = ['cx_Oracle', 'os', 'sqlite3','traceback']
--- python/study/python13/python_oracle.py
+++ python/study/python13/python_oracle.py
@@ -27,82 +27,123 @@ def con():
     return conn, curs
 
 
-def select(curs, sql=''):
-    if sql is '':
+def select(curs, para=['',[]]):
+    '''
+    查询
+    :param curs:
+    :param para: 元组，（SQL,(values)）
+    :type para:tuple
+    :return:
+    '''
+    sql, values = para
+    if not sql:
         raise ValueError('SQL语句不可为空')
     else:
-        curs.execute(sql)
+        if not values:
+            curs.execute(sql)
+        else:
+            curs.execute(sql,values)
         # fetchall() 取出所有数据
         # fetchone() 取出单条数据
         return curs.fetchall()
 
 
-def insert(conn, curs, value, sql=''):
+def insert(conn, curs, values = [], sql=''):
     '''
-    SQL插入，现在只能插入一条数据
+    SQL插入，
     :param conn:
     :param curs:
-    :param value:
+    :param values:类型为列表，每条数据使用列表存放
+    :type values:list
     :param sql:
     :return:
     '''
-    if sql is '':
+    if not sql:
         raise ValueError('SQL语句不能为空')
     else:
-        rows = curs.execute(sql, value)
-        conn.commit()#不提交,无法插入
-        return rows
+        if not values:
+            raise ValueError('SQL value is error')
+        else:
+            try:
+                if type(values[0]) is list:
+                    curs.executemany(sql,values)
+                else:
+                    curs.execute(sql, values)
+                conn.commit()#不提交,无法插入
+            except Exception as e:
+                conn.rollback()
+                traceback.print_exc()
 
 
-def update(conn, curs, value, sql=''):
+def update(conn, curs, value=[], sql=''):
     '''
     更新数据
     :param conn:
     :param curs:
     :param value:
+    :type value:list
     :param sql:
     :return:
     '''
-    if sql is '':
+    if not sql:
         raise ValueError('SQL语句不能为空')
     else:
-        rows = curs.execute(sql, value)
-        conn.commit()
-        return rows
+        if not value:
+            raise ValueError('SQL value is error')
+        else:
+            try:
+                curs.execute(sql, value)
+                conn.commit()
+            except Exception as e:
+                conn.rollback()
+                traceback.print_exc()
 
 
-def delete(conn, curs, value, sql=''):
-    if sql is '':
+def delete(conn, curs, value=[], sql=''):
+    '''
+    :param conn:
+    :param curs:
+    :param value:
+    :type value: list
+    :param sql:
+    :return:
+    '''
+    if not sql:
         raise ValueError('SQL语句不能为空')
     else:
-        rows = curs.execute(sql, value)
-        conn.commit()
-        return rows
+        if not value:
+            raise ValueError('SQL value is error')
+        else:
+            try:
+                curs.execute(sql, value)
+                conn.commit()
+            except Exception as e:
+                conn.rollback()
+                traceback.print_exc()
 
 
 def main():
     try:
-        print(cx_Oracle.paramstyle)
         conn, curs = con()
         # 查询
         # result = select(curs,'''''')
         # for line in result:
         #     print(result)
         # 插入
-        # insert_sql = 'insert into respcode_map(respcode_transmit,channel_transmit,respcode,txstatus,message,flag)' \
-        #             'values(:1, :2, :3, :4, :5, :6)'
-        # rows = insert(conn, curs, ('123', 'ESB', 'a', 'b', '11', '1'), insert_sql)
+        insert_sql = 'insert into respcode_map(respcode_transmit,channel_transmit,respcode,txstatus,message,flag)' \
+                    'values(:1, :2, :3, :4, :5, :6)'
+        values = []
+        values.append(('123', 'ESB', 'a', 'b', '11', '1'))
+        # values.append(('124', 'ESB', 'a', 'b', '11', '1'))
+        insert(conn, curs, ['123', 'ESB', 'a', 'b', '11', '1'], insert_sql)
         # 修改
         # update_sql = 'update respcode_map set respcode = :1 WHERE respcode_transmit = :2'
         # rows = update(conn,curs,('错误', '123'),update_sql)
         # 删除
-        delete_sql = 'delete from respcode_map WHERE respcode_transmit = :1'
-        rows = delete(conn,curs,('123',), delete_sql)
-        print('受影响行数为{}'.format(rows))
-        print(cx_Oracle.paramstyle)
+        # delete_sql = 'delete from respcode_map WHERE respcode_transmit = :1'
+        # delete(conn,curs,('123',), delete_sql)
     except (ValueError,Exception) as e:
-        print(e)
-        conn.rollback()
+        traceback.print_exc()
     finally:
         conn.close()
 
