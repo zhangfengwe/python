01c930d0a89dcd22d548313876c629cf9a925b50
fwzhang <2372590227@qq.com>
2019-08-21 18:29:40 +0800

    python-menu菜单优化以及模块对象测试

--- python/study/python14/python_urllib/urllib_test.py
+++ python/study/python14/python_urllib/urllib_test.py
@@ -9,9 +9,10 @@ import python.study.python11.python_file as fil
 def open_url(url, file):
     data = urlopen(url)
     print(data)
-    openconf = dict(mode='ab+',breflag='N')
-    num = fil.writeFile(file,data,openconf)
+    openconf = dict(mode='ab+', breflag='N')
+    num = fil.writeFile(file, data, openconf)
     return num
+
 
 def open_urlretrieve(url, file):
     result = urlretrieve(url, file)
@@ -19,8 +20,8 @@ def open_urlretrieve(url, file):
 
 
 def main():
-    # print(open_url('http://www.baidu.com', 'test1.html'))
-    print(open_urlretrieve('https://www.taobao.com/', 'test2.html'))
+    print(open_url('https://blog.csdn.net/u012232730/article/details/71078284', 'test3.html'))
+    # print(open_urlretrieve('https://blog.csdn.net/u012232730/article/details/71078284', 'test3.html'))
 
 if __name__ == '__main__':
     main()
