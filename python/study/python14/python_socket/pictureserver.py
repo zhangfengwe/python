9b67f1e5c82b6edf0a49929c86e19f4f611d011d
zhangfengwe <2372590227@qq.com>
2019-09-08 18:07:05 +0800

    python-socket多线程优化

--- python/study/python14/python_socket/pictureserver.py
+++ python/study/python14/python_socket/pictureserver.py
@@ -6,6 +6,7 @@ from socket import socket
 from json import dumps
 from base64 import b64encode
 from time import sleep
+from os import path
 
 class TransPictureServer(Thread):
 
@@ -14,11 +15,14 @@ class TransPictureServer(Thread):
         self._client = cclient
 
     def run(self):
-        my_dict = {}
-        my_dict['filename'] = self._client.recv(1024).decode('gbk')
-        file = open(my_dict['filename'],'rb')
-        data = b64encode(file.read()).decode('gbk')
+        my_dict = dict()
+        filename = self._client.recv(1024).decode('gbk')
+        if not path.isfile(filename):
+            raise FileNotFoundError('文件不存在')
+        with open(filename,'rb') as file:
+            data = b64encode(file.read()).decode('gbk')
         my_dict['filedata'] = data
+        # 将字典转换为json字符串
         json_str = dumps(my_dict)
         self._client.send(json_str.encode('gbk'))
         sleep(10)
@@ -31,6 +35,7 @@ def main():
     server = socket()
     server.bind(('127.0.0.1', 9002))
     server.listen(512)
+    server.settimeout(5)
     print('服务器启动开始监听。。。。')
 
 
--- python/study/python14/python_socket/transpictureclient.py
+++ python/study/python14/python_socket/transpictureclient.py
@@ -13,18 +13,21 @@ def main():
     client.connect(('127.0.0.1',9002))
     client.send(filename.encode('gbk'))
     in_data = bytes()
+    # 读取接收服务器的响应数据
     data = client.recv(1024)
     while data:
         in_data += data
         data = client.recv(1024)
-
+    # 将返回的json字符串转换为字典
     my_dict = loads(in_data.decode('gbk'))
     filedata = my_dict['filedata'].encode('gbk')
 
+    # 判断路径是否存在，不存在，创建
     if not path.exists(filepath+'/'):
         makedirs(filepath+'/')
 
     with open(filepath+'/'+filename, 'wb') as f:
+        # 将服务器返回的base64格式数据解码为二进制数据写入文件
         f.write(b64decode(filedata))
 
     print('图片已保存')
