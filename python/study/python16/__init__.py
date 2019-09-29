4c5dac1f3503ab16aef49bd71b344ad87e2bed5b
fwzhang <2372590227@qq.com>
2019-09-29 18:37:37 +0800

    python-matplotlib学习

---
+++ python/study/python16/__init__.py
@@ -0,0 +1,3 @@
+
+# zip方法并行迭代，zip(list1, list2)
+# zip(*ziped)与zip(list1, list2)相反
--- python/study/python16/matplotlib_line.py
+++ python/study/python16/matplotlib_line.py
@@ -7,19 +7,101 @@ import numpy as np
 
 
 def main():
+    # example_1()
+    # example_2()
+    # example_3()
+    # test()
+    # example_4()
+    example_5()
+
+
+def example_1():
     # 获取数据
     t = np.arange(0.0, 2.0, 0.01)
     s = 1 + np.sin(2 * np.pi * t)
+    # ax2的数据
+    input_values = [0, 1, 2, 3, 4, 5]
+    squares = [0, 1, 4, 9, 16, 25]
 
-    fig, ax = plt.subplots()
-    ax.plot(t, s, )
+    fig, (ax, ax2) = plt.subplots(nrows=1, ncols=2)
+    ax.plot(t, s)
+    ax.set(xlabel='time (s)', ylabel='voltage (mV)', title='matplotlib_example_ax')
+    # ax.set(linewidth=5)
 
-    ax.set(xlabel='time (s)', ylabel='voltage (mV)', title='About as simple as it gets, folks')
+    ax2.plot(input_values, squares)
+    ax2.set(title='matplotlib_example_ax2', xlabel='num', ylabel='num^2')
+    # ax.grid()
 
-    ax.grid()
-
-    fig.savefig('test.png')
+    fig.savefig('example_1.png')
     plt.show()
+
+
+def example_2():
+    input_values = [0, 1, 2, 3, 4, 5]
+    squares = [0, 1, 4, 9, 16, 25]
+    plt.figure()
+    plt.plot(input_values, squares, linewidth=5)
+    plt.title('matplotlib_example_2')
+    plt.ylabel('score')  # 显示y轴的标签
+    plt.xlabel('num')  # 用于显示x轴的标签
+    plt.axis([-5, 10, -5, 25])  # 表示于x轴的显示范围，和y轴的显示范围
+    plt.savefig('example_2')  # 将画出的图片保存下来PNG文件
+    plt.show()
+
+
+def example_3():
+    fig, ax = plt.subplots()
+    x = np.linspace(0, 10, 200)
+    data_obj = {'x': x, 'y1': 2 * x + 1, 'y2': 3 * x + 1.2,
+                'mean': 0.5 * x * np.cos(2*x) + 2.5 * x + 1.1}
+
+    ax.fill_between('x', 'y1', 'y2', color='yellow', data=data_obj)
+
+    ax.plot('x', 'mean', color='black', data=data_obj)
+    fig.savefig('example_3')
+    plt.show()
+
+
+def example_4():
+    x = np.arange(10)
+    y = np.random.randn(10)
+    fig, ax = plt.subplots()
+    ax.scatter(x, y, color='red')
+    fig.savefig('example_4')
+    plt.show()
+
+
+def example_5():
+    np.random.seed(1)
+    x = np.arange(10)
+    y = np.random.randn(10)
+
+    fig, axes = plt.subplots(ncols=2, figsize=plt.figaspect(1/2))
+
+    # 水平条形图 返回artists数组，可以用于操控每个条形图
+    vert_bars = axes[0].bar(x, y, color='lightblue', align='center')
+    # 垂直条形图
+    horiz_bars = axes[1].barh(x, y, color='lightblue', align='center')
+
+    # 在水平或垂直方向上画线
+    axes[0].axhline(0, color='gray', linewidth=2)
+    axes[1].axvline(0, color='gray', linewidth=2)
+
+    for bars in (vert_bars, horiz_bars):
+        for bar, height in zip(bars, y):
+            if height < 0:
+                bar.set(edgecolor='darkred', color='salmon', linewidth=3)
+
+    fig.savefig('example_5')
+    plt.show()
+
+
+def test():
+    data = [('apples', 2), ('oranges', 3), ('peaches', 1)]
+    fruit, value = zip(*data)
+    print(value)
+    print(fruit)
+
 
 if __name__ == '__main__':
     main()
